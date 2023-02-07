76a35da693a2a38d176237ee41c6b93d6927b02c
YifuGuan <yifuguan97@gmail.com>
2023-02-07 11:19:44 +0800

    2023/2/7——引入日志工具slf4j，并成功试用

--- src/main/java/com/dss/juser/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/dss/juser/service/impl/UserInfoServiceImpl.java
@@ -6,6 +6,7 @@ import com.dss.juser.pojo.dto.UserInfoDTO;
 import com.dss.juser.pojo.param.LoginParam;
 import com.dss.juser.pojo.request.LoginRequest;
 import com.dss.juser.service.UserInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,12 @@ import org.springframework.stereotype.Service;
  * @author 刀氏春秋
  * @date 2023/2/3
  */
+@Slf4j
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
     @Autowired
     private LoginMapper loginMapper;
+
     @Override
     public UserInfoDTO confirmUserIdentity(LoginRequest loginRequest) {
         // 请求参数转为查询参数对象
@@ -26,13 +29,15 @@ public class UserInfoServiceImpl implements UserInfoService {
 
         // 依照用户名密码校验身份信息
         UserInfoDO res = loginMapper.queryUserInfoByNameAndPassword(loginInfo);
-        if(res != null){
+        if (res != null) {
             // 包装返回结果
             UserInfoDTO userInfo = new UserInfoDTO();
-            BeanUtils.copyProperties(res,userInfo);
+            BeanUtils.copyProperties(res, userInfo);
+            log.info("已成功验证用户：" + userInfo.getName() + "身份信息");
             return userInfo;
         } else {
             // 验证失败返回
+            log.info("身份信息验证失败，用户名或密码错误");
             return null;
         }
     }
